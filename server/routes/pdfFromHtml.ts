1a1a70bce58a365e9e3194f78072d4c5bde69c97
Bastian Friedrich <bastian.friedrich@cheers.io>
2023-01-25 01:42:57 +0100

    try & error

--- server/routes/pdfFromHtml.ts
+++ server/routes/pdfFromHtml.ts
@@ -1,29 +1,35 @@
-import puppeteer from 'puppeteer';
+// import edgeChromium from 'chrome-aws-lambda';
+// import puppeteer from 'puppeteer-core';
 
-export default defineEventHandler(async () => {
-  const html = await useStorage().getItem('assets:server:template.html');
+// const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+// export default defineEventHandler(async () => {
+//   //  Open chrome
+//   const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
   
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
+//   const browser = await puppeteer.launch({
+//     executablePath,
+//     args: edgeChromium.args,
+//     headless: true,
+//   });
 
-  const page = await browser.newPage();
+//   const page = await browser.newPage();
 
-  //  Add content
-  await page.setContent(html, { waitUntil: 'domcontentloaded' });
+//   //  Add content
+//   await page.setContent('hewo', { waitUntil: 'domcontentloaded' });
 
-  //  Set screen instead of print css
-  // await page.emulateMediaType('screen');
+//   //  Set screen instead of print css
+//   await page.emulateMediaType('screen');
 
-  //  Create pdf
-  const pdf = await page.pdf({
-    margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-		format: 'A4',
-		printBackground: true
-	})
+//   //  Create pdf
+//   const pdf = await page.pdf({
+//     margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+//     format: 'a4',
+//     printBackground: true
+// })
 
-  // Close the browser instance
-  await browser.close();
+//   // Close the browser instance
+//   await browser.close();
 
-  return pdf;
-})
+//   return pdf;
+// })
--- server/routes/test.ts
+++ server/routes/test.ts
@@ -1,29 +1,43 @@
-// import edgeChromium from 'chrome-aws-lambda';
-// import puppeteer from 'puppeteer-core';
+import edgeChromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+import fs from 'fs';
 
-// const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
-// export default defineEventHandler(async () => {
-//   const html = await useStorage().getItem('assets:server:template.html');
+export default defineEventHandler(async (event) => {
+  //  Open chrome
+  const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
 
-//   const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+  const files = [];
+
+  const path = process.env.VERCEL ? '/tmp' : '/Applications/Google Chrome.app/Contents/MacOS/';
+
+  fs.readdirSync(path).forEach(file => {
+    console.log(file);
+    const stat =  fs.lstatSync(path + '/' + file);
+
+    files.push(file + ' : ' + stat.mode + ', '+ stat.uid + ', ' + stat.size);
+  });
+
+  return process.version
+
+  return files;
   
-//   const browser = await puppeteer.launch({
-//     args: [...edgeChromium.args, "--hide-scrollbars", "--disable-web-security"],
-//     defaultViewport: edgeChromium.defaultViewport,
-//     executablePath,
-//     headless: true,
-//     ignoreHTTPSErrors: true,
-//   });
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: true,
+    ignoreDefaultArgs: ['--disable-extensions'],
+  });
 
-//   const page = await browser.newPage();
+  const page = await browser.newPage();
 
-//   await page.goto("https://www.google.com");
+  //  Go to page
+  await page.goto("https://www.google.com");
+  const title = await page.title();
 
-//   const title = await page.title();
+  // Close the browser instance
+  await browser.close();
 
-//   // Close the browser instance
-//   await browser.close();
-
-//   return title;
-// })
+  return title;
+})
